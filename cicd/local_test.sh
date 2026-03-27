b317200eea502c4390ed1758b6d5c54e14e9e770
jhparker7 <github@jhparker.uk>
2026-03-27 16:01:40 +0000

    feat(all): clean slate and adding git commit BREAKING CHANGE: rewrite

--- cicd/local_test.sh
+++
@@ -1,9 +0,0 @@
-docker build -t gatekeeper:test src/gatekeeper
-cd ../..
-cd infra/local
-docker compose up
-cd ../..
-pytest tests/integration_tests
-docker compose down -
-pytest tests/integration_tests
-docker compose down -v
--- sql/auth/setup.sh
+++
@@ -1,3 +0,0 @@
-psql -c 'CREATE DATABASE codeforge'
-
-psql -d 'codeforge' -f docker-entrypoint-initdb.d/setup.sql_script
